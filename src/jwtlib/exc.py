301b5edc4d55b7a99b36273170658d2b8a4c8563
Mateusz Klos <novopl@gmail.com>
2023-06-03 03:58:10 +0100

    change: Remove status code from exception class.
    Leave it up to the caller to decide what to do when a JwtError is
    raised.
    
    Closes JWTLIB-8

--- src/jwtlib/exc.py
+++ src/jwtlib/exc.py
@@ -1,62 +1,40 @@
 """ Exception classes used by jwtlib. """
+from typing import Optional
 
 
 class JwtError(Exception):
-    """ Base exception class for all exceptions thrown by jwtlib. """
-    error = 'Generic Error'
-    message = 'Unknown error'
-    status = 401
+    msg: str
 
-    def __init__(self, message=None, headers=None, status=None):
-        self.error = self.__class__.error or JwtError.error
-        self.message = message or self.__class__.message
-        self.status = status or self.__class__.status
-        self.headers = headers
+    def __init__(self, detail: Optional[str] = None, **kw):
+        super(JwtError, self).__init__(self.msg + (f": {detail}" if detail else ""))
 
-    def __repr__(self):
-        return '<JwtError: {}>'.format(self.error)
-
-    def __str__(self):
-        return 'JwtError: {}: {}'.format(self.error, self.message)
+        self.detail = detail
+        self.err_kwargs = kw
 
 
 class AuthHeaderMissingError(JwtError):
-    """ Raised if authorization header is missing. """
-    error = 'Authorization Header Missing'
-    message = "You should set 'Authorization: Token <token>' header."
+    msg = 'Authorization Header Missing'
 
 
 class BadAuthHeaderError(JwtError):
-    """ Authorization header cannot be parsed. """
-    error = 'Bad Authorization header'
-    message = 'Bad Authorization header'
+    msg = 'Bad Authorization header'
 
 
 class ClaimMissing(JwtError, ValueError):
-    """ Required claim is missing from the token. """
-    error = 'JWT Claim Missing'
+    msg = 'JWT Claim Missing'
 
 
 class InvalidTokenError(JwtError):
-    """ Generic exception for situations where the given toke nis not valid. """
-    error = "Not Authorized"
+    msg = "Not Authorized"
 
 
 class NotAuthorizedError(JwtError):
-    """ User is not authorized to perform the given action. """
-    error = "Not Authorized"
+    msg = "Not Authorized"
 
 
 class UserNotFoundError(JwtError):
-    """ User represented by the token was not found
-
-    User may have been deleted since the token was issued.
-    """
-    error = 'User Not Found'
-    message = 'User does not exist'
+    msg = 'User Not Found'
 
 
 class TokenExpired(JwtError):
-    """ The token passed to .authorize() is expired. """
-    error = 'Token expired'
-    message = 'Token expired'
+    msg = 'Token expired'
