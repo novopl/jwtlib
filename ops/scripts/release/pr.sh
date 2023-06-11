7579862bf3cb0dadfecfed225d333d3a2609f2fd
Mateusz Klos <novopl@gmail.com>
2023-06-11 15:12:15 +0100

    dev: Remove peltak gitflow integration

--- ops/scripts/release/pr.sh
+++
@@ -1,6 +0,0 @@
-# peltak:
-#   about: Create PR for the current release branch
-gh pr create \
-  --repo novopl/jwtlib \
-  --title "Release: v$(peltak version --porcelain)" \
-  --body "$(peltak changelog)"
